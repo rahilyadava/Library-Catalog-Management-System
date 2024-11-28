b27781a70b1870b3f6a642658dc6af25d1fe2b66
divineprecious <divinepreciousesue@gmail.com>
2024-11-28 00:38:23 -0600

    Getting Used to Version Control

--- Library-Catalog-Management-System/main.cpp
+++ Library-Catalog-Management-System/main.cpp
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    qDebug() << "Hello World";
+    qDebug() << "Hello Bob";
     return a.exec();
 }
